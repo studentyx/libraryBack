307ed3c81f72407847ef9c456762c7bf261a9d30
studentyx <tfmuniversity@protonmail.com>
2018-06-18 20:27:07 +0200

    Add Cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   const PORT = 3000;
   await app.listen(PORT, () => {
     console.log(`___________ Run server in localhost:${PORT} ___________`);
