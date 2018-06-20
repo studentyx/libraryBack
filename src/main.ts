616a973c6857ba43bd72492e459dcbaa2461427a
studentyx <tfmuniversity@protonmail.com>
2018-06-20 16:14:47 +0200

    Use content-filter

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cors = require('cors');
 import helmet = require( 'helmet' );
+import bodyParser = require( 'body-parser');
+import filter = require( 'content-filter' );
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
   app.use(helmet());
+
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({extended:true}))
+  app.use(filter());
+
   const PORT = 3000;
   await app.listen(PORT, () => {
     console.log(`___________ Run server in localhost:${PORT} ___________`);
