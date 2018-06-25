318afeec1ee967864cc2da582136f9e81242e207
studentyx <tfmuniversity@protonmail.com>
2018-06-25 22:30:28 +0200

    Update Book API Test

--- src/book/test/book.controller.spec.ts
+++ src/book/test/book.controller.spec.ts
@@ -5,58 +5,160 @@ const port: string = '3000';
 const peticion = request.agent('http://localhost:' + port);
 var id = '';
 
-describe('Testing Book Controller GET /books method', () => {
+var auth = require('../../auth/test/auth.controller.spec.ts');
+var token = '';
+var id = '';
 
-    it('GET /books', (done) => {
-        peticion.get('/books')
-            .end((err, res) => {
-                expect(200).to.equal(res.status);
-                expect('5afb933efc14c31c4071fb71').to.equal(res.body[0]._id);
-                done();
-            });
-    });
-});
+var user = {
+    username: 'diego',
+    password: 'diego',
+};
 
-describe('Testing Book Controller POST /books method', function () {
+describe('Testing Book API\n', function () {
 
-    var book = {
-        title: 'The Lion, the Witch and the Wardrobe (The Chronicles of Narnia)',
-        image: 'https://imagessl1.casadellibro.com/a/l/t0/31/9788408057031.jpg',
-        description: 'Four adventurous siblings—Peter, Susan, Edmund, and Lucy Pevensie—step through a wardrobe door and into the land of Narnia, a land frozen in eternal winter and enslaved by the power of the White Witch. But when almost all hope is lost, the return of the Great Lion, Aslan, signals a great change . . . and a great sacrifice.',
-        genre: [],
-        tags: [],
-        author: 'C. S. Lewis',
-        rating: '0'
-    };
+    describe('Get token authorization: bookManager', function () {
 
-    it('POST /books', function (done) {
-        peticion.post('/books')
-            .send(book)
-            .end((err, res) => {
-                expect(201).to.equal(res.status);
-                expect('The Lion, the Witch and the Wardrobe (The Chronicles of Narnia)').to.equal(res.body.title);
-                id = res.body._id;
-                done();
-            });
-
-    });
-});
-
-describe('Testing Book Controller DELETE /books method', function () {
-
-    it('DELETE /books Succesful Deleting', function (done) {
-        peticion.delete('/books/' + id)
-            .end((err, res) => {
-                expect(200).to.equal(res.status);
-                done();
-            });
-
+        it('POST /auth', async function () {
+            const authRes = await auth.authResponse(user);
+            expect(201).to.equal(authRes.status);
+            token = authRes.body.token;
+        });
     });
 
+    describe('Testing Book Controller POST /books method', function () {
+
+        var book = {
+            title: 'Harry Potter and the Chamber of Secrets',
+            image: 'https://images-na.ssl-images-amazon.com/images/I/51Gxl4YBTSL._SX315_BO1,204,203,200_.jpg',
+            description: "Harry Potter's summer has included the worst birthday ever, doomy warnings from a house-elf called Dobby, and rescue from the Dursleys by his friend Ron Weasley in a magical flying car! Back at Hogwarts School of Witchcraft and Wizardry for his second year, Harry hears strange whispers echo through empty corridors - and then the attacks start. Students are found as though turned to stone . Dobby's sinister predictions seem to be coming true.",
+            genre: [],
+            tags: [],
+            author: 'J.K. Rowling',
+            rating: '0'
+        };
+
+        it('POST /books', function (done) {
+
+            peticion.post('/books')
+                .set('Authorization', token)
+                .send(book)
+                .end((err, res) => {
+                    expect(201).to.equal(res.status);
+                    expect('Harry Potter and the Chamber of Secrets').to.equal(res.body.title);
+                    id = res.body._id;
+                    done();
+                });
+        });
+    });
+
+    describe('Testing Book Controller GET /books method', () => {
+
+        it('GET /books', function (done) {
+            peticion.get('/books')
+                .end((err, res) => {
+                    expect(200).to.equal(res.status);
+                    done();
+                });
+        });
+    });
+
+    describe('Testing Book Controller GET /books/id method', () => {
+
+        it('GET /books', function (done) {
+            peticion.get('/books/' + id)
+                .end((err, res) => {
+                    expect(200).to.equal(res.status);
+                    expect('Harry Potter and the Chamber of Secrets').to.equal(res.body.title);
+                    done();
+                });
+        });
+    });
+
+    describe('Testing Book Controller PATCH /books method', function () {
+
+        var book = {
+            title: '(Modified) Harry Potter and the Chamber of Secrets',
+        };
+
+        it('PATCH /books', function (done) {
+
+            peticion.put('/books/' + id)
+                .set('Authorization', token)
+                .send(book)
+                .end((err, res) => {
+                    expect(200).to.equal(res.status);
+                    expect('(Modified) Harry Potter and the Chamber of Secrets').to.equal(res.body.title);
+                    done();
+                });
+        });
+    });
+
+    describe('Testing Book Controller DELETE /books method without token', function () {
+        it('DELETE /books Must return response 403', function (done) {
+            peticion.delete('/books/' + id)
+                .end((err, res) => {
+                    expect(403).to.equal(res.status);
+                    done();
+                });
+
+        });
+    });
+
+    describe('Testing Book Controller DELETE /books method with token', function () {
+        it('DELETE /books Succesful Deleting', function (done) {
+            peticion.delete('/books/' + id)
+                .set('Authorization', token)
+                .end((err, res) => {
+                    expect(200).to.equal(res.status);
+                    done();
+                });
+
+        });
+    });
+
+    describe('Testing Book Controller DELETE /books method. Id not found', function () {
+        it('DELETE /books Id not found. Response 404', function (done) {
+            peticion.delete('/books/' + id)
+                .set('Authorization', token)
+                .end((err, res) => {
+                    expect(404).to.equal(res.status);
+                    done();
+                });
+
+        });
+    });
+
+
+    describe('Testing Book Controller GET /books/id method Not Found', () => {
+
+        it('GET /books', function (done) {
+            peticion.get('/books/' + id)
+                .end((err, res) => {
+                    expect(404).to.equal(res.status);
+                    done();
+                });
+        });
+    });
+
+    describe('Testing Book Controller PATCH /books method Not Found', function () {
+
+        var book = {
+            title: '(Modified) Harry Potter and the Chamber of Secrets',
+        };
+
+        it('PATCH /books Id not found. Response 404', function (done) {
+
+            peticion.put('/books/' + id)
+                .set('Authorization', token)
+                .send(book)
+                .end((err, res) => {
+                    expect(404).to.equal(res.status);
+                    done();
+                });
+        });
+    });
+
 });
-
-
-
 
 
 
