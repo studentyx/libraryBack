b68c8d1b2b0f86b0c54d49dabd043d609b88cfb7
studentyx <tfmuniversity@protonmail.com>
2018-06-30 02:52:39 +0200

    Format document

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -10,29 +10,27 @@ import * as bcrypt from 'bcrypt';
 export class UserService {
   constructor( @InjectModel('User') private readonly userModel: Model<User>) { }
 
-  async hashPassword (user): Promise<any> {
-    
-      const password = user.password;
-      const saltRounds = 10;
-    
-      const hashedPassword = await new Promise((resolve, reject) => {
-        bcrypt.hash(password, saltRounds, function(err, hash) {
-          if (err) reject(err)
-          resolve(hash);
-        });
-      })
-    
-      return hashedPassword;
-    }
+  async hashPassword(user): Promise<any> {
+    const password = user.password;
+    const saltRounds = 10;
 
-    
+    const hashedPassword = await new Promise((resolve, reject) => {
+      bcrypt.hash(password, saltRounds, function (err, hash) {
+        if (err) reject(err)
+        resolve(hash);
+      });
+    })
+
+    return hashedPassword;
+  }
+
   async create(userDto: UserDto): Promise<User> {
     const condition = { username: userDto.username };
     let userCount = await this.userModel.count(condition).exec();
 
     if (userCount <= 0) {
       const saltRounds = 10;
-      userDto.password = await bcrypt.hash( userDto.password, saltRounds );
+      userDto.password = await bcrypt.hash(userDto.password, saltRounds);
       userDto.rol = 'visitor';
       const user = new this.userModel(userDto);
       return await user.save();
@@ -43,8 +41,8 @@ export class UserService {
 
   async findByUsername(username: string, selectPassword: boolean): Promise<User> {
     let query = this.userModel.findOne({ username: username });
-    if ( selectPassword === true ){
-      query.select( '+password' );
+    if (selectPassword === true) {
+      query.select('+password');
     }
     const user: User = await query.exec();
     return user;
@@ -54,7 +52,7 @@ export class UserService {
     let user: User = null;
 
     const userDB: User = await this.userModel.findOne({ username }).exec();
-    if ( userDB !== null ){
+    if (userDB !== null) {
       userDto.username = userDB.username;
       userDto.rol = userDB.rol;
       if (userDto.password) {
